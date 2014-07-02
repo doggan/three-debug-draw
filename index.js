ce899f50a7fbdd49145297c0ffa699b9ca4359f1
Shyam Guthikonda <shyamguth@gmail.com>
2014-07-02 16:12:11 +0900

    Change color representation.

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 var renderer = require('./lib/renderer'),
     Primitive = renderer.Primitive;
 
-function _drawLine(v0, v1) {
+function stringToColor(str) {
+    switch (str) {
+    case 'red':
+        return 0xFF0000;
+    case 'green':
+        return 0x00FF00;
+    case 'blue':
+        return 0x0000FF;
+    case 'yellow':
+        return 0xFFFF00;
+    case 'magenta':
+        return 0xFF00FF;
+    case 'cyan':
+        return 0x00FFFF;
+    case 'white':
+        return 0xFFFFFF;
+    default:
+        return 0x000000;
+    }
+}
+
+function toColor(arg) {
+    if (typeof arg == 'string') {
+        return stringToColor(arg);
+    } else {
+        return arg;
+    }
+}
+
+function _drawLine(v0, v1, color) {
     var p = new Primitive();
+
     p.vertices = [v0, v1];
-    p.color = { r: 0xFF, g: 0, b: 0 };
+    p.color = toColor(color);
+    
     renderer.addPrimitive(p);
 }
 
